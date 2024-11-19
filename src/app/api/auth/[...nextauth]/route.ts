64cdb2e8a1c5b752026f50ec2e5dcb866165d6ec
laszloscheers <laszloscheers.gmail.com>
2024-11-19 14:02:03 +0100

    Atempt to fix auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -71,51 +71,50 @@ const handler = NextAuth({
       session.user = token as any;
       return session;
     },
-    async signIn({ account, profile }) {
-      if (account?.provider === "google") {
-        // check if user is in your database
-        if (profile?.email) {
-          const userInDB = await GetUserByEmail(
-            `google_account_${profile?.email}`
-          );
-          if (userInDB.error) {
-            // add your user in DB here with profile data (profile.email, profile.name)
-            await SignUpAction({
-              name: profile?.name ?? "",
-              surname: profile?.name ?? "",
-              email: `google_account_${profile?.email}`,
-              password: process.env.GOOGLE_AND_GITHUB_ACCOUNTS_PASSWORD ?? "",
-            });
-          }
-        }
-        return true;
-      }
+    // async signIn({ account, profile }) {
+    //   if (account?.provider === "google") {
+    //     // check if user is in your database
+    //     if (profile?.email) {
+    //       const userInDB = await GetUserByEmail(
+    //         `google_account_${profile?.email}`
+    //       );
+    //       if (userInDB.error) {
+    //         // add your user in DB here with profile data (profile.email, profile.name)
+    //         await SignUpAction({
+    //           name: profile?.name ?? "",
+    //           surname: profile?.name ?? "",
+    //           email: `google_account_${profile?.email}`,
+    //           password: process.env.GOOGLE_AND_GITHUB_ACCOUNTS_PASSWORD ?? "",
+    //         });
+    //       }
+    //     }
+    //     return true;
+    //   }
 
-      if (account?.provider === "github") {
-        // check if user is in your database
-        if (profile?.email) {
-          const userInDB = await GetUserByEmail(
-            `github_account_${profile?.email}`
-          );
-          if (userInDB.error) {
-            // add your user in DB here with profile data (profile.email, profile.name)
-            await SignUpAction({
-              name: profile?.email ?? "",
-              surname: profile?.email ?? "",
-              email: `github_account_${profile?.email}`,
-              password: process.env.GOOGLE_AND_GITHUB_ACCOUNTS_PASSWORD ?? "",
-            });
-          }
-        }
-      }
-      return true;
-    },
+    //   if (account?.provider === "github") {
+    //     // check if user is in your database
+    //     if (profile?.email) {
+    //       const userInDB = await GetUserByEmail(
+    //         `github_account_${profile?.email}`
+    //       );
+    //       if (userInDB.error) {
+    //         // add your user in DB here with profile data (profile.email, profile.name)
+    //         await SignUpAction({
+    //           name: profile?.email ?? "",
+    //           surname: profile?.email ?? "",
+    //           email: `github_account_${profile?.email}`,
+    //           password: process.env.GOOGLE_AND_GITHUB_ACCOUNTS_PASSWORD ?? "",
+    //         });
+    //       }
+    //     }
+    //   }
+    //   return true;
+    // },
   },
   pages: {
     signIn: "/login",
     error: "/error",
   },
-  debug: true,
 });
 
 export { handler as GET, handler as POST };
