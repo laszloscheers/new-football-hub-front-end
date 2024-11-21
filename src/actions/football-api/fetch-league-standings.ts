6c21d4e212ab49b405f957ccfe7ad87ecc4e1e92
laszloscheers <laszloscheers.gmail.com>
2024-11-22 00:12:32 +0100

    trying to fetch league data

--- src/actions/football-api/fetch-league-standings.ts
+++ src/actions/football-api/fetch-league-standings.ts
@@ -17,7 +17,8 @@ export const fetchLeagueStandings = async (league: string) => {
           headers: {
             "Content-Type": "application/json",
             "X-Auth-Token": apiKeys[i] || "",
-            "access-control-allow-origin": "http://localhost",
+            "access-control-allow-origin":
+              process.env.NEXT_PUBLIC_APP_URL + "*" || "",
           },
         }
       );
